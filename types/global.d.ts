b0a65ad955ef8cf4d2dc4a64a06b01c884c452f7
betocantu93 <betocantu93@gmail.com>
2024-01-04 23:39:52 +0100

    m

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,18 +1,7 @@
 // Types for compiled templates
-declare module '@addons/prysmex-ui/templates/*' {
+declare module 'demo/templates/*' {
   import type { TemplateFactory } from 'htmlbars-inline-precompile';
   const tmpl: TemplateFactory;
   export default tmpl;
 }
 
-declare module 'ember-moment/services/moment' {
-  import Service from '@ember/service';
-
-  import type { Moment } from 'moment';
-
-  class MomentService extends Service {
-    moment(str?: string | Date): Moment;
-  }
-
-  export default MomentService;
-}
